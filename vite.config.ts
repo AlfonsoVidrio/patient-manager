bb55eafe4f005ad1065d79190453f2f5d148dbd0
AlfonsoVidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2026-07-10 23:47:01 -0600

    chore: add gh-pages dependency and deploy configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/patient-manager/",
   plugins: [
     react(),
     tailwindcss(),
